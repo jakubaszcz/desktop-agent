7e32a4608bdaf80486655b32f31c8884dcce6536
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-12 16:21:33 +0200

    feat: add the heartbeat into a thread

--- machine/src/main.rs
+++ machine/src/main.rs
@@ -1,14 +1,31 @@
 use std::{thread, time};
-use tungstenite::connect;
+use std::sync::mpsc;
+use tungstenite::{connect, Utf8Bytes};
 
 fn main() {
+    let (tx, rx) = mpsc::channel();
+
     let (mut socket, _) = connect("ws://localhost:8080/heartbeat")
         .expect("Failed to connect");
 
+    thread::spawn(move || {
+        let (mut socket, _) = connect("ws://localhost:8080/heartbeat")
+            .expect("Failed to connect");
+
+        loop {
+            socket.send(tungstenite::Message::Text(
+                Utf8Bytes::from(r#"{"type":"heartbeat"}"#.to_string())
+            )).unwrap();
+
+            if let Ok(keybind) = rx.try_recv() {
+                socket.send(tungstenite::Message::Text(keybind)).unwrap();
+            }
+
+            thread::sleep(time::Duration::from_secs(5));
+        }
+    });
+ 
     loop {
-        socket.send(tungstenite::Message::text("heartbeat")).unwrap();
-        let msg = socket.read().unwrap();
-        println!("Go dit: {}", msg);
-        thread::sleep(time::Duration::from_secs(5));
+        // tx.send("message").unwrap();
     }
 }
