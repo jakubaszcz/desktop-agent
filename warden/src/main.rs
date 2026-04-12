e444dfd3fa5179d0310d4340adf665e887e8445a
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-13 00:12:56 +0200

    feat: detect a new file from the downloads

--- warden/src/main.rs
+++ warden/src/main.rs
@@ -1,7 +1,37 @@
 use std::{thread, time};
+use std::path::PathBuf;
+use std::sync::mpsc;
+use notify::{recommended_watcher, Event, EventKind, Watcher};
 use tungstenite::{connect, Utf8Bytes};
 
+fn watcher(root: PathBuf, download_dir: PathBuf) {
+    let (tx, rx) = mpsc::channel();
+
+    let mut warden = recommended_watcher(move |res| {
+        tx.send(res).unwrap();
+    }).unwrap();
+
+    warden.watch(&download_dir, notify::RecursiveMode::NonRecursive).unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(Ok(Event { kind: EventKind::Create(_), paths, .. })) => {
+                println!("{:?}", paths);
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
+
+    let root = dirs::home_dir().unwrap().join("my-desktop-agent");
+    let download_dir = dirs::download_dir().unwrap();
+
+    {
+        std::fs::create_dir_all(&root).unwrap();
+    }
+
     thread::spawn(move || {
        let (mut socket, _) = connect("ws://localhost:8080/warden").expect("Failed to connect to warden");
 
@@ -14,5 +44,5 @@ fn main() {
         }
     });
 
-    loop {}
+    watcher(root, download_dir);
 }
