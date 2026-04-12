3171693e089f8509c55bcdf99bca23ec1f64cade
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-12 19:13:54 +0200

    feat: connect warden client to server

--- warden/src/main.rs
+++ warden/src/main.rs
@@ -1,3 +1,18 @@
+use std::{thread, time};
+use tungstenite::{connect, Utf8Bytes};
+
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(move || {
+       let (mut socket, _) = connect("ws://localhost:8080/warden").expect("Failed to connect to warden");
+
+        loop {
+            socket.send(tungstenite::Message::Text(
+                Utf8Bytes::from(r#"{"type":"heartbeat", "from":"warden"}"#.to_string())
+            )).unwrap();
+
+            thread::sleep(time::Duration::from_secs(5));
+        }
+    });
+
+    loop {}
 }
