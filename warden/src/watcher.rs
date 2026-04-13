80c1a1ebe18db87bbe9b08ff446bf5fbe34602f2
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-13 13:52:42 +0200

    feat: separate warden into more files

---
+++ warden/src/watcher.rs
@@ -0,0 +1,26 @@
+﻿use std::path::PathBuf;
+use std::sync::mpsc;
+use notify::{recommended_watcher, Event, EventKind, Watcher};
+use crate::manager;
+
+pub(crate) fn watch(root: PathBuf, download_dir: PathBuf) {
+    let (tx, rx) = mpsc::channel();
+
+    let mut warden = recommended_watcher(move |res| {
+        tx.send(res).unwrap();
+    }).unwrap();
+
+    warden.watch(&download_dir, notify::RecursiveMode::NonRecursive).unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(Ok(Event { kind: EventKind::Create(_), paths, .. })) => {
+                for path in paths {
+                    std::thread::sleep(std::time::Duration::from_millis(500));
+                    manager::manage(&path, &root);
+                }
+            }
+            _ => {}
+        }
+    }
+}
