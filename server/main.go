24a5f7788fa8f3f5a4d2f589cf170a889c3e8938
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-15 20:59:20 +0200

    feat: add the stray to shut down server

--- server/main.go
+++ server/main.go
@@ -2,7 +2,31 @@ package main
 
 import (
 	"server/routes"
+
+	"github.com/getlantern/systray"
 )
+
+func onReady() {
+	systray.SetTitle("Desktop agent")
+	systray.SetTooltip("Desktop agent")
+
+	// Create the button to shut down the server
+	mShutDown := systray.AddMenuItem("Shutdown", "Shutdown the desktop agent")
+
+	go func() {
+		for {
+			select {
+			case <-mShutDown.ClickedCh:
+				systray.Quit()
+				return
+			}
+		}
+	}()
+}
+
+func onExit() {
+	// Empty
+}
 
 func main() {
 
@@ -13,6 +37,10 @@ func main() {
 		routes.Window()
 	}
 
+	// Create the stray to close the desktop agent
+	systray.Run(onReady, onExit)
+
 	// Server
 	routes.Server()
+
 }
