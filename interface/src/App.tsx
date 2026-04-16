d00dc002b081e2346b09b4478d0e399f0c3aed90
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-16 15:43:48 +0200

    feat: separate main content and navigation sections

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -2,11 +2,28 @@ import "./App.css";
 
 function App() {
 
-  return (
-    <main className="container">
-      <p>Hello, world !</p>
-    </main>
-  );
+    return (
+        <main style={
+            {
+                display: "flex",
+                height: "100vh",
+            }}>
+            <div style={
+                {
+                    width: "30%",
+                    background: "#1e1e1e"
+                }}>
+                {/* This is where the sidebar will go */}
+            </div>
+            <div style={
+                {
+                    flex: 1,
+                    background: "#f5f5f5"
+                }}>
+                {/* All the content will go here */}
+            </div>
+        </main>
+    );
 }
 
 export default App;
